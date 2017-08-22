574d63d1fa83cd837c32d0440a84509f77bb5ea6
Vin Busquet <computationalcore@gmail.com>
2017-08-22 12:37:38 -0300

    feat: Add authenticated messages service and tests

--- src/services/index.js
+++ src/services/index.js
@@ -2,7 +2,10 @@
 
 const users = require('./users/users.service.js');
 
+const messages = require('./messages/messages.service.js');
+
 module.exports = function() {
   const app = this; // eslint-disable-line no-unused-vars
   app.configure(users);
+  app.configure(messages);
 };
